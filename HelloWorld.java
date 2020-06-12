f536d7ec524053238ea0bed007a3dc337b072c06
Caivan <caivan2@gmail.com>
2020-06-12 18:59:55 -0500

    Addin new lines to test the build with jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,6 +6,12 @@ public class HelloWorld {
 
     public static void printHelloWorld () {
         System.out.println("Hello World friends!!!!");
+        System.out.println("Hello World friends new line 1");
+        System.out.println("Hello World friends new line 2");
+        System.out.println("Hello World friends new line 3");
+        System.out.println("Hello World friends new line 4");
+        System.out.println("Hello World friends new line 5");
+        System.out.println("Hello World friends new line 6");
     }
 
 }
