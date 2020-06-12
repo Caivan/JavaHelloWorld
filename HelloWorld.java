63b62eba8888a1bf41383cb12c221b95679de163
Caivan <caivan2@gmail.com>
2020-06-12 17:05:20 -0500

    Generate class hello world

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public static void main (string args []) {
+        printHelloWorld();
+    }
+
+    public static void printHelloWorld () {
+        System.out.println("Hello World friends!!!!");
+    }
+
+}
